794beebebaf34992471fe43c170505cca80ff8c8
Alan Lim <60864183+Bun27Lim@users.noreply.github.com>
2022-04-05 12:19:48 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
 
-  RAPID_API_KEY: 'e0f1a65e82msh0b8260066073ecfp1fa6a5jsn5aa941f149fe',
+  RAPID_API_KEY: '',
   RAPID_API_HOST: 'rawg-video-games-database.p.rapidapi.com',
 
-  RAWG_API_KEY: '0fa8cd8999ac4e899f90b807e80657c7'
+  RAWG_API_KEY: ''
 };
 
 /*
